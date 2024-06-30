661d214ed38a8988449eb3644f3d342d62ea3d7a
yoo16 <yohei.yoshikawa@gmail.com>
2024-06-30 22:40:34 +0900

    update

--- app/api/chat/route.tsx
+++ app/api/chat/route.tsx
@@ -1,20 +1,25 @@
 // app/api/chat/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { GoogleGenerativeAI } from '@google/generative-ai';
+import { Message } from '@/app/interfaces/Message';
 
 export async function POST(req: NextRequest) {
-    const { message } = await req.json();
+    const message = await req.json();
     if (!message) return;
+
+    // const botMessage:Message = { sender: "bot", content: "Hello" };
+    // return NextResponse.json(botMessage);
 
     const API_KEY = process.env.GEMINI_API_KEY;
     if (!API_KEY) return;
-
     try {
         const genAI = new GoogleGenerativeAI(API_KEY);
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-        const result = await model.generateContent(message);
-        const data = result.response.text();
-        return NextResponse.json({ reply: data });
+        const result = await model.generateContent(message.content);
+        const content = result.response.text();
+        const botMessage:Message = { sender: "bot", content: content };
+
+        return NextResponse.json(botMessage);
     } catch (error) {
         return NextResponse.json({ error: 'GoogleGenerativeAI error' }, { status: 500 });
     }
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,72 @@
 'use client';
 
 import { useState } from "react";
+import { Message } from "./interfaces/Message";
+import axios from "axios";
 
 export default function Home() {
-  const [input, setInput] = useState<string>('');
+    const [inputMessage, setInputMessage] = useState<string>('');
+    const [messages, setMessages] = useState<Message[]>([]);
 
-  return (
-    <main className="flex flex-col items-center justify-between p-6">
-      <h1 className="text-2xl p-3">Gemini Chatbot</h1>
+    const changeMessageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+        setInputMessage(e.target.value);
+    };
 
-      <div className="mt-3">
-        <input
-          type="text"
-          className="flex-grow p-2 border border-gray-300 rounded mr-2"
-          placeholder="Type your message..."
-        />
-        <button className="bg-blue-500 text-white p-2 rounded">Send</button>
-      </div>
+    const sendHandler = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        if (inputMessage.trim() === '') return;
 
-    </main>
-  );
+        const userMessage: Message = { sender: 'user', content: inputMessage };
+        const updatedMessages = [...messages, userMessage];
+        setMessages(updatedMessages);
+
+        try {
+            const response = await axios.post('/api/chat', userMessage);
+            const botMessage = response.data;
+            setMessages([...updatedMessages, botMessage]);
+        } catch (error) {
+            console.error('Error sending message:', error);
+        }
+
+        setInputMessage('');
+    };
+
+    return (
+        <main className="flex flex-col justify-center p-6">
+            <h1 className="text-2xl p-3">Gemini Chatbot</h1>
+
+            <div className="mt-3">
+                <input
+                    onChange={changeMessageHandler}
+                    value={inputMessage}
+                    type="text"
+                    className="w-full flex-grow p-2 border border-gray-300 rounded mr-2"
+                    placeholder="Type your message..."
+                />
+                <button
+                    className="bg-blue-500 text-white p-2 rounded"
+                    onClick={sendHandler}>
+                    Send
+                </button>
+            </div>
+
+            <div className="p-4 mb-4 overflow-y-scroll">
+                {messages && messages.map((message, index) => (
+                    <div
+                        key={index}
+                        className={`m-3 p-5 border border-gray-100 rounded
+                            ${message.sender === 'user' ?
+                                'text-right text-blue-600' :
+                                'text-left text-gray-600'}
+                            `}
+                    >
+                        <span>
+                            {message.sender}: {message.content}
+                        </span>
+                    </div>
+                ))}
+            </div>
+
+        </main>
+    );
 }
